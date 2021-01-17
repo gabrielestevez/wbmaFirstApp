5d85befb13a06d56f3612ac23d0c0c3f45586276
Gabriel Estevez Cabello <g.esteves@usp.ceu.es>
2021-01-17 20:58:56 +0200

    Ajax C

--- App.js
+++ App.js
@@ -10,41 +10,14 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-
-const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
+import {useLoadMedia} from './hooks/ApiHooks';
 
 // let mediaArray = [];
 
 const uploadsUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
 const App = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadMedia = async (limit = 5) => {
-    try {
-      const listResponse = await fetch(baseUrl + 'media?limit=' + limit);
-      const listJson = await listResponse.json();
-      console.log('response json data', listJson);
-
-      const media = await Promise.all(
-        listJson.map(async (item) => {
-          const fileResponse = await fetch(baseUrl + 'media/' + item.file_id);
-          const fileJson = fileResponse.json();
-          // console.log('media file data', fileJson);
-          return fileJson;
-        })
-      );
-      console.log('media array data', media);
-
-      setMediaArray(media);
-    } catch (error) {
-      console.error('loadMedia error', error);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia();
-  }, []);
+  const mediaArray = useLoadMedia();
 
   return (
     <SafeAreaView style={styles.container}>

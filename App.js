0ba452c34fd2df2760739670b058aa359e572944
Gabriel Estevez Cabello <g.esteves@usp.ceu.es>
2021-01-17 19:38:09 +0200

    Ajax A

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import {StatusBar} from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   FlatList,
   Image,
@@ -11,44 +11,35 @@ import {
   View,
 } from 'react-native';
 
-const mediaArray = [
-  {
-    key: '0',
-    title: 'Title 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/161',
-    },
-    filename: 'http://placekitten.com/2048/1920',
-  },
-  {
-    key: '1',
-    title: 'Title 2',
-    description:
-      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/164',
-    },
-    filename: 'http://placekitten.com/2041/1922',
-  },
-  {
-    key: '2',
-    title: 'Title 3',
-    description:
-      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/167',
-    },
-    filename: 'http://placekitten.com/2039/1920',
-  },
-];
+const url =
+  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+
+// let mediaArray = [];
 
 const App = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log('response json data', json);
+      setMediaArray(json);
+    } catch (error) {
+      console.error('loadMedia error', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+  // loadMedia();
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={mediaArray}
+        keyExtractor={(item, index) => index.toString()}
         renderItem={({item}) => {
           return (
             <TouchableOpacity>
@@ -99,16 +90,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'steelblue',
   },
 });
-
-const Flexboxdemo = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor="orange" barStyl="light-content" />
-      <View style={styles.header} />
-      <View style={styles.infoArea} />
-      <View style={styles.footerArea} />
-    </SafeAreaView>
-  );
-};
 
 export default App;
